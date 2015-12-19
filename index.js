c1b3fa81d74df10026a36b93822c2297c93f58ab
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-19 21:45:47 +0100

    add regex escaping - fixes #1

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
+var escape = require('reg-escape');
 module.exports = function (input, search) {
 	if (typeof input !== 'string') {
 		throw new TypeError('The input should be a string');
 	}
 
 	return [].concat(search).reduce(function (count, search) {
-		return count + (input.match(new RegExp(search, 'ig')) || []).length;
+		return count + (input.match(new RegExp(escape(search), 'ig')) || []).length;
 	}, 0);
 };
--- test.js
+++ test.js
@@ -12,4 +12,5 @@ test('occurrences', t => {
 	t.is(fn('foo foo bar', 'foo'), 2);
 	t.is(fn('foo foo bar', ['foo', 'bar']), 3);
 	t.is(fn('foo bar\nfoo baz', 'foo'), 2);
+	t.is(fn('(foo) [bar]', '(foo)'), 1);
 });
